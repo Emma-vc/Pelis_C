6675514fa82499e2b902421fb04782bf1852248d
Vero_ <emma.svc@gmail.com>
2025-06-03 20:41:06 -0300

    Primer commit: subiendo el proyecto al repositorio local

---
+++ Peliculas.py
@@ -0,0 +1,79 @@
+from inicio import login,inicio_historial,acciones,catalogo_flyer, menu_principal
+from datetime import datetime
+
+#### POO + encapsulacion
+# class pelicula:
+#     def __init__(self):
+#         pass
+    
+# class catalogoPeliculas:
+#     def __init__(self, nombre, ruta_archivo):
+#         pass   
+#     def agregar_pelicula(self):
+#         pass
+#     def listar_peliculas(self):
+#         pass
+#     def eliminar_pelicula(self):
+#         pass
+
+### funcion principal, que engloba a las del py inicio 
+def app():
+    usuario_valido = login() # a usuario lo busco en la funcion login para que salga del ambito local y se redefina en un ambito global creo
+
+    if usuario_valido:
+        # la funcion inicio.. puede usar el usuario
+        inicio_historial(usuario_valido)
+        # llamado a la funcion catalogo       
+        
+
+        # bucle principal del menú de opciones
+        while True:
+            catalogo_flyer()
+            opcion=menu_principal() 
+           
+            if opcion == 1:
+                print(">>> Agregando película...")
+                # Llamar a la función para agregar película aquí
+            elif opcion == 2:
+                print(">>> Listando películas...")
+                # Llamar a la función para listar películas aquí
+            elif opcion == 3:
+                print(">>> Eliminando catálogo...")
+                # Llamar a la función para eliminar catálogo aquí
+            elif opcion == 4:
+                # Opción SALIR
+                hora_salida = datetime.now().strftime("%H:%M")
+                mensaje_salida = f"Finalizó la sesión a las {hora_salida}"
+                
+                # 'acciones' es la misma lista que importada. Añadir aquí.
+                acciones.append(mensaje_salida)
+                
+                # 'usuario_valido' está definido y disponible en este punto
+                with open(f"historial_{usuario_valido}.txt", "a") as archivo:
+                    archivo.write(mensaje_salida + "\n")
+                
+                print(">>> Saliendo del programa. ¡Hasta luego!")
+                break # Salir del bucle del menú
+            elif opcion == 5:
+                # Opción para ver el historial
+                print("\n--- Historial de Sesión ---")
+                if not acciones:
+                    print("El historial de esta sesión está vacío.")
+                else:
+                    for accion in acciones:
+                        print(accion)
+                # print("---------------------------\n")
+                RED_FG = "\x1B[31m"
+                RESET = "\x1B[0m"
+                print(f"{RED_FG}{'*'*50}{RESET}")
+            else:
+                print("Opción no válida. Por favor, intente de nuevo.")
+    else:
+        # Este 'else' solo se ejecutaría si tu función 'login()'
+        # tuviera una forma de retornar `None` o `False` (por ejemplo,
+        # si el usuario falla el login después de X intentos).
+        print("\n 🚩 🚩 Error en el inicio de sesión 🚩 🚩 Saliendo de la aplicación....... ")
+
+# para que el programa solo se ejecute en el archivo principal
+if __name__ == "__main__":
+    app()# Inicia toda la aplicación    
---
+++ inicio.py
@@ -0,0 +1,126 @@
+from datetime import datetime
+
+## FUNCION Login estan contempladas las respectivas validaciones al ingreso, codigo ansi.  No crea un usuario, permite el ingreso de usuarios ya registrados
+ 
+def login():
+    personas = {"andrea": 6385, "nazarena": 8196, "roberta": 4221, "juan": 3697, "martin":2532}
+    print("")
+    CYAN = "\x1B[36m"
+    RESET = "\x1B[0m"
+    print(f"{CYAN}{'*'*11}{RESET} INICIAR SESION {CYAN}{'*'*11}{RESET}\n")
+    
+    max_intentos=3
+    intentos_usuario=0
+    usuario_valido = None
+
+    ##Bucle 1 de validacion de ingreso para el usuario 
+    while intentos_usuario < max_intentos:
+        usuario = input("Ingrese su nombre de usuario: ").lower()
+        if not usuario.isalpha():
+            print("Solo puede ingresar letras --> 🅰️ 🅱️")
+            continue
+        
+        if usuario not in personas:
+            print("Usuario no registrado")
+            intentos_usuario=intentos_usuario+1
+            print(f"Quedan {max_intentos-intentos_usuario} intentos!!")
+            if intentos_usuario==max_intentos:
+                break # fin del limite de intentos 
+            continue 
+        ##usuario existente
+        usuario_valido=usuario
+        break
+    ## Se terminaron los intentos para el usuario 
+    if usuario_valido is None:
+        RESET = "\x1B[0m"  
+        ITALIC = "\x1B[3m"
+        RED = "\x1B[31m" 
+        print(f"\nLa cantidad de intentos máximos {max_intentos} fue superada por el usuario!!\n") 
+        print(f"{RED}{ITALIC}  Acceso denegado!!!{RESET}")
+        return None
+    
+    ### Bucle 2 de validacion de ingreso para la clave 
+    intentos_clave=0   
+    while intentos_clave < max_intentos:
+        try:
+            clave = int(input("Ingrese su contraseña: "))
+            if clave==personas[usuario_valido]:
+               GREEN = "\x1B[32m"
+               RESET = "\x1B[0m"  
+               ITALIC = "\x1B[3m"
+               BOLD= "\x1B[1m"
+               WHITE = "\x1B[37m"
+               print(f"\n {GREEN}{ITALIC}Acceso permitido{RESET} \n")
+               usuario_valido=usuario.capitalize()
+               print(f" {BOLD}{WHITE}¡Hola! {usuario_valido}{RESET}")
+               return usuario_valido
+            else:
+                 print("Contraseña incorrecta. Vuelva a intentar")
+                 intentos_clave=intentos_clave+1
+                 print(f"Quedan {max_intentos-intentos_clave} intentos!!")
+        except ValueError:
+                print("Por favor, ingrese solo números 1️⃣ 2️⃣ 3️⃣ ")
+                intentos_clave=intentos_clave+1
+                print(f"Quedan {max_intentos-intentos_clave} intentos!!")
+                
+    RESET = "\x1B[0m"  
+    ITALIC = "\x1B[3m"
+    RED = "\x1B[31m" 
+    print(f"\n La cantidad de intentos maximos {max_intentos} fue superada!!\n") 
+    print(f"{RED}{ITALIC}  Acceso denegado!!!{RESET}")           
+
+### FUNCION de SEGUIMIENTO inciamos un seguimiento del usuario desde que ingresa, empezamos con el encabezado con su nombre y fecha y lo guardamos en el txt 
+acciones=[]
+def inicio_historial(usuario_valido):
+    timestamp = datetime.now().strftime("%A %d-%m-%Y %H:%M")
+    encabezado= f"{usuario_valido} ingreso el día {timestamp}:"
+    acciones.append(encabezado)
+    with open(f"historial_{usuario_valido}.txt", "a") as archivo:
+        archivo.write("\n" + encabezado + "\n")
+
+#Por cada usuario se va hacer un historial de lo que hizo desde que ingreso, si elimino agrego algo, se uso del enconding para evitar errores en el titulo de peliculas
+def registrar_accion(usuario_valido, actividad):
+    registro = f"- {actividad}"
+    acciones.append(registro)
+    with open(f"historial_{usuario_valido}.txt", "a", encoding="utf-8") as archivo:
+        archivo.write(registro + "\n")
+##ver historial opcion 5
+def mostrar_historial(usuario_valido):
+    try:
+        with open(f"historial_{usuario_valido}.txt", "r", encoding="utf-8") as archivo:
+            print(archivo.read())
+    except FileNotFoundError:
+        print(f" Todavia no hay un historial para {usuario_valido}")
+
+###FUNCION INICIO CATALOGO
+def catalogo_flyer():
+      ITALIC = "\x1B[3m"
+      BOLD = "\x1B[1m" 
+      BRIGHT_CYAN_FG = "\x1B[96m"
+      RESET = "\x1B[0m"
+      print(f"\n {BRIGHT_CYAN_FG}{BOLD}{'⁓⁘'*8} C U E V A N A {ITALIC} New g {'⁓⁘'*8}{RESET}\n")
+    #   catalogo_movies= input("Ingrese el nombre del catalogo de peliculas: ")
+    #   if catalogo_movies.strip():
+    #       return catalogo_movies
+    #   else:
+    #      print("Por favor, intente nuevamente 😊")  
+         
+
+## FUNCION MENU
+def menu_principal():
+    
+    opciones={ 1: "AGREGAR PELICULA 🎞️",
+               2: "LISTAR PELICULAS 🧾",
+               3: "ELIMINAR CATALOGO DE PELICULA 🗑️",
+               4: "SALIR 🏃🏽‍♂️",
+               5: "Ver historial registrado  🔍"}
+    
+    BRIGHT_MAGENTA = "\x1B[95m"
+    BOLD = "\x1B[1m"
+    RESET = "\x1B[0m"
+    WHITE = "\x1B[37m"
+    print(f"\n {BRIGHT_MAGENTA}{'⁘⁓'*5}{RESET}{BOLD}{WHITE} SELECCIONE UNA OPCION {RESET} {BRIGHT_MAGENTA}{'⁓⁘'*5}{RESET}\n")
+    for numero,opcion in opciones.items():
+        print(f"{numero}.{opcion}")
+    opcion=int(input("\nOpcion elegida: "))
+    return opcion    
